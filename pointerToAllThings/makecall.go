a89cd928fa08dc56bb46f61a7b11b9e5edb16b3f
Joel Hill <joel@invisionapp.com>
2017-07-24 12:00:54 -0700

    clean up

--- pointerToAllThings/makecall.go
+++ pointerToAllThings/makecall.go
@@ -10,6 +10,8 @@ func makeAntiPatternPointerCalls() bool {
 
 	// if not nil checked, could throw null pointer error.
 	if incomingRequest.ID1 != nil && *incomingRequest.ID1 > 1 {
+
+		// do something with an int
 		return true
 	}
 	return false
@@ -21,6 +23,9 @@ func makeBetterPointerCalls() bool {
 	incomingRequest := withlesspointers.SomeModel{}
 
 	// no need to check for nil because its defaulted to 0
+
+	// do something with an int
+
 	if incomingRequest.ID1 > 1 {
 		return true
 	}
